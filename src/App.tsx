42c00ecea9b1e62fb19215dc06a4324c283f37fd
Brijesh Kumar <BrijeshKum@hcl.com>
2019-10-11 17:00:05 +0530

    eod commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,63 @@
 import React, { useState } from 'react';
 import firebase from 'firebase';
-import { signInAnonymously, writeData, readData } from './api';
-import { Container, Message, List } from 'semantic-ui-react';
+import app from './api/firebase';
+import { signInAnonymously, writeData, readData, signInWithGoogle } from './api';
+import { Container, Message, List, Segment, Dimmer, Loader, Button } from 'semantic-ui-react';
+
+export interface DataResult {
+  error: string;
+  data: Array<firebase.firestore.QueryDocumentSnapshot>;
+  loading: boolean;
+  user: firebase.User | null;
+}
 
 const App: React.FC = () => {
 
+  const user = app.auth().currentUser;
+
+  const initialState = {
+    error: '',
+    data: new Array<firebase.firestore.QueryDocumentSnapshot>(),
+    loading: false,
+    user: user
+  }
+
   //console.log(firebase.auth().currentUser);
-  const [user, setUser] = useState<firebase.User>();
-  const [error, setError] = useState<string>();
-  const [data, setData] = useState<firebase.firestore.QueryDocumentSnapshot[]>();
+  const [dataResult, setDataResult] = useState(initialState);
 
-  //console.log(process.env.REACT_APP_API_KEY);
-  
-  signInAnonymously(setError);
+  //signInAnonymously(setError);
+  //signInWithGoogle(dataResult, setDataResult);
   //writeData('test', { name: 'brijesh kumar' }, setError);
-  readData('test', setData, setError);
-  //console.log(app.auth().currentUser);
+  readData('test', dataResult, setDataResult);
 
-  //firebaseApp.auth().signInAnonymously().then(onFulfilled).catch(onRejected);
+  const renderLogin = () => {
+    return <Button primary
+      onClick={() => signInWithGoogle(dataResult, setDataResult)}>Login with Google</Button>
+  }
+
+  const renderList = () => {
+    return <Segment>
+      <Message
+        className={dataResult.error ? 'error' : 'success'}
+        header={dataResult.error ? 'Ops' : 'Welcome'}
+        content={dataResult.error} />
+      <List>
+        {dataResult.data ? dataResult.data.map(item => <List.Item key={item.id}>{JSON.stringify(item.data())}</List.Item>) : null}
+      </List>
+    </Segment>;
+  }
 
   return (
+
     <Container>
-      <Message
-        className={error ? 'error' : 'success'}
-        header={error ? 'Ops' : 'Welcome'}
-        content={error} />
-      <List>
-        {data ? data.map(item => <List.Item key={item.id}>{JSON.stringify(item.data())}</List.Item>) : null}
-      </List>
+      <Segment>
+        <Dimmer className={dataResult.loading ? 'active inverted' : ''}>
+          <Loader>Loading</Loader>
+        </Dimmer>
+        {dataResult.user ? renderList() : renderLogin()}
+
+      </Segment>
+
     </Container>
   );
 }
--- src/api/index.ts
+++ src/api/index.ts
@@ -1,26 +1,84 @@
 import app from './firebase';
 import firebase from 'firebase';
+import { DataResult } from '../App';
 
 export interface TestData {
     name: string;
 }
 
-export const signInAnonymously = (setError: React.Dispatch<React.SetStateAction<string | undefined>>): void => {
-    app.auth().signInAnonymously().catch((error) => {
-        setError(error);
+export const signInAnonymously = (dataResult: DataResult, setDataResult: React.Dispatch<React.SetStateAction<DataResult>>): void => {
+
+    dataResult.loading = true;
+    setDataResult({ ...dataResult });
+    app.auth().signInAnonymously().then(() => {
+        dataResult.loading = false;
+        setDataResult({ ...dataResult });
+    }).catch((error) => {
+        dataResult.loading = false;
+        dataResult.error = error;
+        setDataResult({ ...dataResult });
     });
 }
 
-export const writeData = (collectionName: string, data: TestData, setError: React.Dispatch<React.SetStateAction<string | undefined>>): void => {
-    app.firestore().collection(collectionName).add(data).catch(error => {
-        setError(error);
+export const signInWithGoogle = (dataResult: DataResult,
+    setDataResult: React.Dispatch<React.SetStateAction<DataResult>>): void => {
+
+    dataResult.loading = true;
+    setDataResult({ ...dataResult });
+    const provider = new firebase.auth.GoogleAuthProvider();
+    app.auth().signInWithPopup(provider).then((result) => {
+        if (result.credential) {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            const token = result.credential.providerId;
+        }
+        // The signed-in user info.
+        const user = result.user;
+        dataResult.loading = false;
+        dataResult.user = user;
+        setDataResult({ ...dataResult });
+    }).catch(function (error) {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        const credential = error.credential;
+
+        dataResult.loading = false;
+        dataResult.error = error;
+        setDataResult({ ...dataResult });
     });
 }
 
-export const readData = (collectionName: string, setData: React.Dispatch<React.SetStateAction<firebase.firestore.QueryDocumentSnapshot[] | undefined>>, setError: React.Dispatch<React.SetStateAction<string | undefined>>): void => {
-    app.firestore().collection(collectionName).get().then((data) => {
-        setData(data.docs)
+export const writeData = (collectionName: string, data: TestData, dataResult: DataResult,
+    setDataResult: React.Dispatch<React.SetStateAction<DataResult>>): void => {
+
+    dataResult.loading = true;
+    setDataResult({ ...dataResult });
+    app.firestore().collection(collectionName).add(data).then(() => {
+        dataResult.loading = false;
+        setDataResult({ ...dataResult });
     }).catch(error => {
-        setError(error);
+        dataResult.loading = false;
+        dataResult.error = error;
+        setDataResult({ ...dataResult });
+    });
+}
+
+export const readData = (collectionName: string,
+    dataResult: DataResult,
+    setDataResult: React.Dispatch<React.SetStateAction<DataResult>>): void => {
+
+    //dataResult.loading = true;
+    //setDataResult({ ...dataResult });
+    app.firestore().collection(collectionName).get().then((data) => {
+        dataResult.loading = false;
+        dataResult.data = data.docs;
+        setDataResult({ ...dataResult });
+    }).catch(error => {
+        dataResult.loading = false;
+        dataResult.error = error;
+        setDataResult({ ...dataResult });
     });
 }
